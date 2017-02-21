6aa9dfcbd43260066aa23d6abc802c059ba7ee62
Brett Strydom <bstrydo@thoughtworks.com>
2017-02-21 11:29:47 +0200

    added

---
+++ i18n.php
@@ -0,0 +1,50 @@
+<?php
+
+/**
+ * Define the internationalization functionality
+ *
+ * Loads and defines the internationalization files for this plugin
+ * so that it is ready for translation.
+ *
+ * @link       {link}
+ * @since      1.0.0
+ *
+ * @package    {package_name}
+ */
+
+/**
+ * Define the internationalization functionality.
+ *
+ * Loads and defines the internationalization files for this plugin
+ * so that it is ready for translation.
+ *
+ * @since      1.0.0
+ * @package    {package_name}
+ * @author     {author}
+ */
+class i18n {
+
+
+    private $plugin_name;
+	/**
+	 * Load the plugin text domain for translation.
+	 *
+	 * @since    1.0.0
+	 */
+    function __construct($plugin_name) {
+        $this->plugin_name =  $plugin_name;
+    }
+
+	public function load_plugin_textdomain() {
+
+		load_plugin_textdomain(
+			$this->plugin_name,
+			false,
+			dirname( dirname( plugin_basename( __FILE__ ) ) ) . '/languages/'
+		);
+
+	}
+
+
+
+}
