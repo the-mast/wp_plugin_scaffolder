5aac5f7d40218dfa1275c225356b8f4f718eb863
Brett Strydom <bstrydo@thoughtworks.com>
2017-02-21 11:37:34 +0200

    added

---
+++ class-plugin_name-public.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace {plugin_name}_plugin;
+
+class {plugin_name}_Public {
+
+    private $plugin_name;
+
+    private $version;
+    private ${plugin_name}_options;
+
+    public function __construct( $plugin_name, $version, $options ) {
+
+        $this->plugin_name = $plugin_name;
+        $this->version = $version;
+        $this->{plugin_name}_options = $options;
+
+    }
+
+    public function {plugin_name}_action() {
+        if(!empty($this->{plugin_name}_options["{plugin_name}_action"])){
+            return include_once("partials/{plugin_name}-action.php") ;
+        }
+
+        return 0;
+    }
+
+    public function brett_filter() {
+        if(!empty($this->{plugin_name}_options["brett_filter"])){
+            return include_once("partials/brett-filter.php");
+        }
+
+        return 0;
+    }
+
+	public function load_styles() {
+		wp_enqueue_style( $this->plugin_name, plugin_dir_path( __FILE__ ) . "css/{plugin_name}-public.css", array(), $this->version, "all" );
+	}
+
+	public function load_scripts() {
+		wp_enqueue_script( $this->plugin_name, plugin_dir_path( __FILE__ ) . "js/{plugin_name}-public.js", array( "jquery" ), $this->version, false );
+	}
+}
