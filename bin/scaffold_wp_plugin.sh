8654d28545e36906db8dc1684206695917dabe04
Brett Strydom <bstrydo@thoughtworks.com>
2017-02-21 14:45:19 +0200

    added some filtering

--- bin/scaffold_wp_plugin.sh
+++ bin/scaffold_wp_plugin.sh
@@ -7,6 +7,7 @@ plugin_uri="www.thoughtworks.com/abc"
 version="1.0.0"
 author="Brett"
 author_uri="www.thoughtworks.com"
+plugin_uri=`echo $plugin_uri | sed s/\//\\\//g`
 
 template_dir=` dirname \`dirname $0\` `"/template_files"
 plugin_dir="`pwd`/$plugin_name"
@@ -24,8 +25,21 @@ cd "$plugin_dir"
 tar -xvf template_files.tar
 rm -f template_files.tar
 
+function do_subs() {
+    for sub_filename in `find . -name "*.php"`; do
+
+        sed -i ".plugin_name.bkp" s/"{plugin_name}"/"$plugin_name"/g "$sub_filename"
+        sed -i ".version.bkp" s/"{version}"/"$version"/g "$sub_filename"
+        sed -i ".package_name.bkp" s/"{package_name}"/"$package_name"/g "$sub_filename"
+        sed -i ".link.bkp" s/"{link}"/"$link"/g "$sub_filename"
+        sed -i ".author.bkp" s/"{author}"/"$author"/g "$sub_filename"
+        sed -i ".author_uri.bkp" s/"{author_uri}"/"$author_uri"/g "$sub_filename"
+        sed -i ".plugin_uri.bkp" s/"{plugin_uri}"/"$plugin_uri"/g "$sub_filename"
+    done
+}
+
 function fix_filenames() {
-    for plugin_file in `find . -name "*plugin_name*php"`; do
+    for plugin_file in `find . -name "*plugin_name*"`; do
         src_filename=`echo $plugin_file | sed s/"\.\/"//g`
         target_filename=`echo $src_filename | sed s/plugin_name/"$plugin_name"/g`
 
@@ -33,4 +47,11 @@ function fix_filenames() {
     done
 }
 
+function remove_backup_files() {
+find . -name "*.bkp" -exec rm -f "{}" ";"
+}
+
 fix_filenames
+do_subs
+
+remove_backup_files
