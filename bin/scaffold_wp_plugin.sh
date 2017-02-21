c52be4e8d935d759854f229d4b620c1dd68ba2fa
Brett Strydom <bstrydo@thoughtworks.com>
2017-02-21 13:39:48 +0200

    added filename renaming

--- bin/scaffold_wp_plugin.sh
+++ bin/scaffold_wp_plugin.sh
@@ -8,7 +8,29 @@ version="1.0.0"
 author="Brett"
 author_uri="www.thoughtworks.com"
 
-if [ -d "$plugin_name" ]; then
+template_dir=` dirname \`dirname $0\` `"/template_files"
+plugin_dir="`pwd`/$plugin_name"
+if [ -d "$plugin_dir" ]; then
     echo "directory $plugin_name already exists";
     exit 1
 fi
+
+mkdir "$plugin_dir"
+
+current_dir=`pwd`
+cd "$template_dir"
+tar -vcf "$plugin_dir/template_files.tar" *
+cd "$plugin_dir"
+tar -xvf template_files.tar
+rm -f template_files.tar
+
+function fix_filenames() {
+    for plugin_file in `find . -name "*plugin_name*php"`; do
+        src_filename=`echo $plugin_file | sed s/"\.\/"//g`
+        target_filename=`echo $src_filename | sed s/plugin_name/"$plugin_name"/g`
+
+        mv "$src_filename" "$target_filename"
+    done
+}
+
+fix_filenames
