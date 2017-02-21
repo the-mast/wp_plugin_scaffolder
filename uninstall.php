301c8e4feaa37c2098177aa8d65cc3dba1b16ac9
Brett Strydom <bstrydo@thoughtworks.com>
2017-02-21 11:32:24 +0200

    added, need to change it though

---
+++ uninstall.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Fired when the plugin is uninstalled.
+ *
+ *
+ * @link       {link}
+ * @since      1.0.0
+ *
+ * @package    {package_name}
+ */
+
+// If uninstall not called from WordPress, then exit.
+if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
+	exit;
+}
