c64a7392569b086774cea0eaaacbab6667ed9070
Brett Strydom <bstrydo@thoughtworks.com>
2017-02-24 09:50:04 +0200

    put the functions into a file that gets included for easier testing

--- bin/scaffold_wp_plugin.sh
+++ bin/scaffold_wp_plugin.sh
@@ -16,95 +16,6 @@ else
 fi
 
 
-function get_plugin_dir() {
-    echo "$(pwd)/$plugin_name"
-}
-
-function make_plugin_dir() {
-    if [ -d $plugin_dir ]; then
-        echo "directory $plugin_name already exists";
-        exit 1
-    fi
-
-    mkdir "$plugin_dir"
-}
-
-function do_subs() {
-    if [ ! -d "$plugin_dir" ]; then
-        echo "$plugin_dir doesn't exist can't rename files"
-        return 1
-    fi
-
-    cd "$plugin_dir"
-    for sub_filename in `find . -name "*.php"`; do
-
-        sed -i ".plugin_name.bkp" s/"{plugin_name}"/"$plugin_name"/g "$sub_filename"
-        sed -i ".version.bkp" s/"{version}"/"$version"/g "$sub_filename"
-        sed -i ".package_name.bkp" s/"{package_name}"/"$package_name"/g "$sub_filename"
-        sed -i ".link.bkp" s/"{link}"/"$link"/g "$sub_filename"
-        sed -i ".author.bkp" s/"{author}"/"$author"/g "$sub_filename"
-        sed -i ".author_uri.bkp" s/"{author_uri}"/"$author_uri"/g "$sub_filename"
-        sed -i ".plugin_uri.bkp" s/"{plugin_uri}"/"$plugin_uri"/g "$sub_filename"
-        sed -i ".plugin_uri.bkp" s/"{description}"/"$description"/g "$sub_filename"
-    done
-
-    return 0
-}
-
-function fix_filenames() {
-    if [ ! -d "$plugin_dir" ]; then
-        echo "$plugin_dir doesn't exist can't rename files"
-        return 1
-    fi
-
-    cd "$plugin_dir"
-    for plugin_file in `find . -name "*plugin_name*"`; do
-        src_filename="$(echo $plugin_file | sed s/"\.\/"//g)"
-        target_filename="$(echo $src_filename | sed s/plugin_name/"$plugin_name"/g)"
-
-        mv "$src_filename" "$target_filename"
-    done
-
-    cd "$current_dir"
-    return 0
-}
-
-function remove_backup_files() {
-    if [ ! -d "$plugin_dir" ]; then
-        echo "$plugin_dir doesn't exist can't rename files"
-        return 1
-    fi
-
-    cd "$plugin_dir"
-    find . -name "*.bkp" -exec rm -f "{}" ";"
-
-    return 0
-}
-
-function sed_esc() {
-    echo "$1" | sed s/"\/"/"\\\\\/"/g | sed s/"&"/"\\\&"/g
-}
-
-function copy_template_files_to_plugin_dir() {
-    make_plugin_dir
-    cd "$template_dir"
-    tar -vcf "$plugin_dir/template_files.tar" *
-    cd "$plugin_dir"
-    tar -xvf template_files.tar
-    rm -f template_files.tar
-    cd "$current_dir"
-}
-
-function process_interactive_args() {
-    echo "not yet implemented"
-    exit 0
-}
-
-function process_noninteractive_args() {
-    echo "not yet implemented"
-    exit 0
-}
-
 export plugin_dir="$(pwd)/$plugin_name"
 export template_dir="$scaffold_path/template_files"
 export bin_dir="$scaffold_path/bin"
@@ -112,6 +23,7 @@ export includes_dir="$scaffold_path/includes"
 export current_dir="$(pwd)"
 
 source "$includes_dir/startup_args"
+source "$includes_dir/functions"
 
 case "$1" in
     "-h" | "--help")
